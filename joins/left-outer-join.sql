086f1a588d9a432f89d91364d79f0be813452e77
Craig McKeachie <craigdmckeachie@gmail.com>
2025-06-05 10:46:23 -0400

    left outer join example

---
+++ joins/left-outer-join.sql
@@ -0,0 +1,9 @@
+use northwind;
+
+select 
+	customers.customerid, 
+    companyname,
+    orderdate
+from customers
+left outer join orders on customers.customerid = orders.customerid
+-- where orderdate is null;
